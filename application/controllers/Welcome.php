d31cfb69f0bd8245ce3f92f354ed985dee8fe98d
wangjake <wang_run_sheng@sina.com>
2017-05-23 14:02:18 +0800

    jake
    curl

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+
+	// 构造
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('curl');
+		$this->load->library('session');
+	}
+
+	// 保修列表
 	public function index()
 	{
-		$this->load->view('welcome_message');
+
+		$url = "http://lws.lohas-deco.com/Internal_DD.svc/GetDepartList";
+		$result = $this->curl->doGet($url);
+		header("Content-type:application/json;charset=utf-8");
+		$output_json = json_encode($result);
+		var_dump($output_json);exit;
+
+		echo $output_json;
 	}
 }
